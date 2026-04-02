5104a58d104f88fb1f015d62670731466c244641
Joshua Provoste <8358462+JoshuaProvoste@users.noreply.github.com>
2026-04-02 16:25:24 -0300

    feat: add no-insecure-url-resolution security rule

--- index.js
+++ index.js
@@ -7,12 +7,14 @@
 module.exports = {
   rules: {
     "no-inner-h-t-m-l-binding": require("./lib/rules/no-inner-h-t-m-l-binding"),
+    "no-insecure-url-resolution": require("./lib/rules/no-insecure-url-resolution"),
   },
   configs: {
     recommended: {
       plugins: ["polymer-security"],
       rules: {
         "polymer-security/no-inner-h-t-m-l-binding": "error",
+        "polymer-security/no-insecure-url-resolution": "error",
       },
     },
   },
